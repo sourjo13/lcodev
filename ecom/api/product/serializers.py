47abe4e0065b1876ceb87cbe1596c6e1b5f27139
Sourjo Ganguly <sourjo13@hotmail.com>
2023-06-23 19:43:08 +0530

    First commit

---
+++ ecom/api/product/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Product
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'image', 'category')
+
+
